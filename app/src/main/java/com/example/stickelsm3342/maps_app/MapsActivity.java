d692f0928e6d405a503bc35539a94fbc92d234bc
StickelsM <mtstickels@yahoo.com>
2017-05-26 09:18:23 -0700

    Made most of it work, fixed so many errors

--- app/src/main/java/com/example/stickelsm3342/maps_app/MapsActivity.java
+++ app/src/main/java/com/example/stickelsm3342/maps_app/MapsActivity.java
@@ -1,8 +1,14 @@
 package com.example.stickelsm3342.maps_app;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,6 +19,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    int toggle = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +32,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker and move the camera
-        LatLng sandiego = new LatLng(32, -117);
+        LatLng sandiego = new LatLng(32.7, -117);
         mMap.addMarker(new MarkerOptions().position(sandiego).title("Marker in San Diego"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sandiego));
+
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
     }
+
+    public void mapView(View v) {
+        if (toggle == 0) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            toggle = 1;
+        } else if (toggle == 1) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            toggle = 0;
+        }
+
+    }
+
+
+    public void myLocation(View v) {
+
+
+
+    }
+
 }
